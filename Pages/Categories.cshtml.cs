ee69cd2cfa5e4621a9a93f9113a6447e42f78411
Gabriel Milani <g.milani.e@gmail.com>
2023-10-01 14:53:39 -0300

    Criado layouts de navegações padrões asp.net.

---
+++ Pages/Categories.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages.Pages;
+
+public class Categories : PageModel
+{
+    public List<Category> CategoryList { get; set; } = new();
+    
+    public async Task OnGet(int skip = 0, 
+                            int take = 25)
+    {
+        var temp = new List<Category>();
+        await Task.Delay(1000);
+        for (var i = 0; i < 1700 ; i++)
+        {
+            temp.Add(
+                new Category(i,
+                    $"Categoria {i}",
+                    i * 18.95M
+                )
+            );
+        }
+        CategoryList = temp.Skip(skip)
+                           .Take(take)
+                           .ToList();
+    }
+}
+public record Category(
+    int id,
+    string Title,
+    decimal Price);
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,29 +1,11 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPages.Pages;
 
 public class Index : PageModel
 {
-    public List<Category> Categories { get; set; } = new();
-    
-    public async Task OnGet()
+    public void OnGet()
     {
-        await Task.Delay(5000);
-        for (var i = 0; i < 100 ; i++)
-        {
-            Categories.Add(
-                new Category(i,
-                    $"Categoria {i}",
-                    i * 18.95M
-                )
-            );
-        }
+        
     }
 }
-
-public record Category(
-    int id,
-    string Title,
-    decimal Price);
